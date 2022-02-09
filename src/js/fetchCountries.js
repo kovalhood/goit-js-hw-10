305d4be9ffb554dd4c6ce078e136223e607d7ed4
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-02-09 15:59:23 +0200

    Task

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,13 @@
+const BASE_URL = 'https://restcountries.com/v3.1';
+
+function fetchCountries(name) {
+    return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
+        .then(response => {
+        if (response.status === 404) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    })
+};
+
+export default { fetchCountries };
